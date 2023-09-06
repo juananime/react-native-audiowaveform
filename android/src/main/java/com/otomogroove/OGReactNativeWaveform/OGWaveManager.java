9c4b1ec2624e6a0c16ea9e5076a48def28cc4c13
Shaheer Khalid <shaheerkhalid10@gmail.com>
2023-09-06 18:54:23 +0500

    Android Integration Issue Resolved 6-Sep-2023

--- android/src/main/java/com/otomogroove/OGReactNativeWaveform/OGWaveManager.java
+++ android/src/main/java/com/otomogroove/OGReactNativeWaveform/OGWaveManager.java
@@ -1,6 +1,6 @@
 package com.otomogroove.OGReactNativeWaveform;
 
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.Log;
 
 import com.facebook.react.bridge.Arguments;
